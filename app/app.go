deb428caccd79e73ff186307aef0d5e5c3b9a17b
nplayer89 <n.player89@gmail.com>
2023-09-22 16:10:31 -0600

    add .env and environment variables for config

--- app/app.go
+++ app/app.go
@@ -3,13 +3,22 @@ package app
 import (
 	"banking/domain"
 	"banking/service"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func Start() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Could not load .env")
+	}
+	checkEnv()
+
 	router := mux.NewRouter()
 
 	//wiring
@@ -18,5 +27,31 @@ func Start() {
 	router.HandleFunc("/customers", ch.GetAllCustomers).Methods(http.MethodGet)
 	router.HandleFunc("/customers/{customer_id:[0-9]+}", ch.GetCustomer).Methods(http.MethodGet)
 
-	log.Fatal(http.ListenAndServe("localhost:8000", router))
+	address := os.Getenv("SERVER_ADDRESS")
+	port := os.Getenv("SERVER_PORT")
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", address, port), router))
+}
+
+func checkEnv() {
+	if os.Getenv("SERVER_ADDRESS") == "" {
+		log.Fatal("Environment variable SERVER_ADDRESS is not defined")
+	}
+	if os.Getenv("SERVER_PORT") == "" {
+		log.Fatal("Environment variable SERVER_PORT is not defined")
+	}
+	if os.Getenv("DB_USER") == "" {
+		log.Fatal("Environment variable DB_USER is not defined")
+	}
+	if os.Getenv("DB_PASSWD") == "" {
+		log.Fatal("Environment variable DB_PASSWD is not defined")
+	}
+	if os.Getenv("DB_ADDR") == "" {
+		log.Fatal("Environment variable DB_ADDR is not defined")
+	}
+	if os.Getenv("DB_PORT") == "" {
+		log.Fatal("Environment variable DB_PORT is not defined")
+	}
+	if os.Getenv("DB_NAME") == "" {
+		log.Fatal("Environment variable DB_NAME is not defined")
+	}
 }
--- domain/CustomerRepositoryDB.go
+++ domain/CustomerRepositoryDB.go
@@ -4,6 +4,8 @@ import (
 	"banking/errs"
 	"banking/logger"
 	"database/sql"
+	"fmt"
+	"os"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -37,7 +39,7 @@ func (d CustomerRepositoryDB) ById(id string) (*Customer, *errs.AppError) {
 	customerSql := "SELECT customer_id, name, city, zipcode, date_of_birth, status FROM customers WHERE customer_id = ?"
 
 	var c Customer
- 	err := d.client.Get(&c, customerSql, id)
+	err := d.client.Get(&c, customerSql, id)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return nil, errs.NewNotFoundError("Customer not found")
@@ -50,7 +52,13 @@ func (d CustomerRepositoryDB) ById(id string) (*Customer, *errs.AppError) {
 }
 
 func NewCustomerRepositoryDb() CustomerRepositoryDB {
-	client, err := sqlx.Open("mysql", "root:codecamp@tcp(localhost:3306)/banking")
+	dbUser := os.Getenv("DB_USER")
+	dbPasswd := os.Getenv("DB_PASSWD")
+	dbAddr := os.Getenv("DB_ADDR")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+	dataSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPasswd, dbAddr, dbPort, dbName)
+	client, err := sqlx.Open("mysql", dataSource)
 	if err != nil {
 		panic(err)
 	}
