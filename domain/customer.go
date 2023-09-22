c457802f0a75a1e0603b9dd7b6522e782bfd31c7
nplayer89 <n.player89@gmail.com>
2023-09-22 15:41:09 -0600

    add dto

--- domain/customer.go
+++ domain/customer.go
@@ -1,6 +1,9 @@
 package domain
 
-import "banking/errs"
+import (
+	"banking/dto"
+	"banking/errs"
+)
 
 type Customer struct {
 	Id          string `db:"customer_id"`
@@ -14,4 +17,24 @@ type Customer struct {
 type CustomerRepository interface {
 	FindAll(string) ([]Customer, *errs.AppError)
 	ById(string) (*Customer, *errs.AppError)
+}
+
+func (c Customer) ToDto() dto.CustomerResponse {
+
+	return dto.CustomerResponse{
+		Id:          c.Id,
+		Name:        c.Name,
+		City:        c.City,
+		Zipcode:     c.Zipcode,
+		DateOfBirth: c.DateOfBirth,
+		Status:      c.statusAsText(),
+	}
+}
+
+func (c Customer) statusAsText() string {
+	statusAsText := "active"
+	if c.Status == "0" {
+		statusAsText = "inactive"
+	}
+	return statusAsText
 }
